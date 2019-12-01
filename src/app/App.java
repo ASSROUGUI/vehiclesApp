0bc1fa8baa0227eba48b4c47795cfe575ed0580d
ASSROUGUI <38358417+ASSROUGUI@users.noreply.github.com>
2019-12-02 01:41:37 +0300

    Update App.java

--- src/app/App.java
+++ src/app/App.java
@@ -22,35 +22,72 @@ public class App {
 //        vehicles1.setFuelType("Diesel");
 //        vehicles1.setvehicles(8,4,5,7,-1,8);
 //        vehicles1.setColor("white");
+   System.out.println("welcome to vehicles press 1: Cars 2:Motorcycle 3.Trucks"); 
+
    
-
-//         Scanner input = new Scanner(System.in);
-//       int i = input.nextInt();
-// 
-//Scanner in = new Scanner(System.in);
-//String name = in.nextLine();
-
-
-truck truckObject =new truck();
-    truckObject.welcomeMsg();
-//    truckObject.setColor(name);
-      truckObject.setColor("red");
-     truckObject.setLoadWeight(999);
-//    truckObject.setLoadWeight(i);
-    truckObject.setFuelType("water");
-   
-
     
+         Scanner input = new Scanner(System.in);
+       int  num = input.nextInt();
+  
     car carObject = new car();
-    carObject.welcomeMsg();
-    carObject.setname("car", "honda");
-    carObject.setTorque(99);
-    carObject.setHorsePower(88);
-    
     Motorcycle MotorcycleObject = new Motorcycle();
+    truck truckObject =new truck();
+    
+
+
+switch(num) {
+ case 1:
+   
+    carObject.welcomeMsg();
+    carObject.setname("classic car", "toyota");
+    carObject.setFuelType("Gas");
+    carObject.setColor("white");
+    carObject.setseatsNo(4);
+    carObject.WheelsNo(4);
+    carObject.setYearMake(2012);
+    carObject.setTorque(3);
+    carObject.setRPM(6);
+    carObject.setHorsePower(66);
+        System.out.println("bye"); 
+    break;
+  case 2:
+         
     MotorcycleObject.welcomeMsg();
-    MotorcycleObject.setFuelType("Gas");
-    MotorcycleObject.setseatsNo(-5);
+    MotorcycleObject.setname("superMotorcycle", "toyota");
+    MotorcycleObject.setFuelType("water");
+    MotorcycleObject.setColor("red");
+    MotorcycleObject.WheelsNo(2);
+    MotorcycleObject.setseatsNo(2);
+    MotorcycleObject.setYearMake(2019);
+    MotorcycleObject.setTorque(999);
+    MotorcycleObject.setRPM(2);
+    MotorcycleObject.setHorsePower(106);
+        System.out.println("bye"); 
+  
+    break;
+     case 3:
+         
+    truckObject.welcomeMsg();
+    truckObject.setname("super Truck", "hondai");
+    truckObject.setFuelType("Diesel");
+    truckObject.setColor("black");
+    truckObject.setseatsNo(2);
+    truckObject.WheelsNo(4);
+    truckObject.setYearMake(2016);
+    truckObject.setTorque(0);
+    truckObject.setRPM(-6);
+    truckObject.setHorsePower(218);
+    truckObject.setLoadWeight(999);
+        System.out.println("bye"); 
+  
+    break;
+  default:
+      System.out.println("Not Failed number");
+}
+
+   
+
+
 
     
     }  
