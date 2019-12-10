e0d85d05b1ea9b58ba4ee8bd4b0af77e7c90d37f
ASSROUGUI <38358417+ASSROUGUI@users.noreply.github.com>
2019-12-11 02:55:49 +0300

    Update car.java

--- src/app/car.java
+++ src/app/car.java
@@ -11,11 +11,18 @@ package app;
  */
 public class car  extends vehicles {
    
-     
+    private String newestCar ="2020 Ford Mustang Shelby GT500";
 //    public car(){
 //          System.out.println(" Constructor of car");
 //          
 //    }
+          public void setnewestCars(String cars)
+          { this.newestCar=cars;}
+          
+          public String getnewestCars()
+          {return newestCar;}
+
+    
           public void welcomeMsg(){
    System.out.println("  welcome to Car");}
 }
